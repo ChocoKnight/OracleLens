a811607a2e14cc3252ebc350d43672e12bf877d4
Kevin Tan <k.tann.578@gmail.com>
2025-02-25 21:08:54 -0800

    top bar

--- packages/app/src/components/Navbars/TopBar.tsx
+++ packages/app/src/components/Navbars/TopBar.tsx
@@ -35,18 +35,32 @@ import '../../styles/Navbar.css';
 
 const TopBar: React.FC<{}> = () => {
     return (
-        <Navbar expand="lg" fixed="top" className='navbar'>
-            <Container>
-                <Navbar.Brand href='/'>
-                    <h1>
-                        Oracle Lens
-                    </h1>
-                </Navbar.Brand>
-                <Nav>
+        <div>
+            <Navbar expand="lg" fixed="top" className='title'>
+                <Container>
+                    <Navbar.Brand href='/'>
+                        <h1>
+                            Oracle Lens
+                        </h1>
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Navbar expand="lg" className='navbar'>
+                <Container>
+                    <Nav className = 'nav'>
+                        <Nav.Link href='/Home'>Home</Nav.Link> 
+                        <Nav.Link href='/Tournaments'>Tournaments</Nav.Link> 
+                        <Nav.Link href='/Teams'>Teams</Nav.Link>
+                        <Nav.Link href='/Players'>Players</Nav.Link>
+                        <Nav.Link href='/Champions'>Champions</Nav.Link>
+                        <Nav.Link href='/Rankings'>Rankings</Nav.Link>
+                        <Nav.Link href='/Rankings'>Rankings</Nav.Link>
+                        <Nav.Link href='/Predictions'>Predictions</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
 
-                </Nav>
-            </Container>
-        </Navbar>
     )
 }
 
