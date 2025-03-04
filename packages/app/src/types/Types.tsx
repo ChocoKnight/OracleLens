4220eeee2cddb3aec2ad02da1a69497bac9d8780
Kevin Tan <k.tann.578@gmail.com>
2025-03-03 22:27:07 -0800

    match page rest api update to show needed data

--- packages/app/src/types/Types.tsx
+++ packages/app/src/types/Types.tsx
@@ -143,3 +143,108 @@ export interface TournamentSummary {
     matchList: Match[];
     teams: TeamName[];
 }
+
+export interface MatchInfo {
+    matchId: number;
+    tournamentId: number;
+    league: string;
+    split: string;
+    year: number;
+    date: string;
+    patch: string;
+}
+
+export interface TeamScore {
+    teamId: number;
+    teamName: string;
+    year: number;
+    blueWins: number;
+    redWins: number;
+}
+
+export interface PickBan {
+    gameId: number;
+    matchId: number;
+    gameNumber: number;
+    blueTeamName: string;
+    redTeamname: string;
+    side: string
+    ban1: string;
+    ban2: string;
+    ban3: string;
+    ban4: string;
+    ban5: string;
+    pick1: string;
+    pick2: string;
+    pick3: string;
+    pick4: string;
+    pick5: string;
+}
+
+export interface Objectives {
+    gameId: number;
+    matchId: number;
+    gameNumber: number;
+    blueTeamName: string;
+    redTeamname: string;
+    side: string
+    firstBlood: number;
+    firstTower: number;
+    towers: number;
+    towerPlates: number;
+    voidGrubs: number;
+    riftHeralds: number;
+    baronNashors: number;
+    infernals: number;
+    mountains: number;
+    clouds: number;
+    oceans: number;
+    hextechs: number;
+    chemtechs: number;
+    elders: number;
+    featsOfStrength: number;
+    ruinousAtakhan: number;
+    voraciousAtakhan: number;
+}
+
+export interface PlayerPerformance {
+    gameId: number;
+    matchId: number;
+    gameNumber: number;
+    blueTeamId: number;
+    blueTeamName: string;
+    redTeamId: number
+    redTeamname: string;
+    playerId: number;
+    playerName: string;
+    playerTeam: number;
+    role: string;
+    champion: string;
+    kills: number;
+    deaths: number;
+    assists: number;
+    damageToChampions: number;
+    wardsPlaced: number;
+    wardsDestroyed: number;
+    controlWardsBought: number;
+    visionScore: number;
+    totalGold: number;
+    goldSpent: number;
+    creepScore: number;
+    killsAt15: number;
+    deathsAt15: number;
+    assistsAt15: number;
+    goldAt15: number;
+}
+
+export interface Games {
+    pickBans: PickBan[];
+    objectives: Objectives[];
+    playerPerformances: PlayerPerformance[];
+}
+
+export interface MatchSummary {
+    matchInfo: MatchInfo;
+    teamScores: TeamScore[];
+    games: Games
+}
--- packages/server/src/services/match-svc.ts
+++ packages/server/src/services/match-svc.ts
@@ -1,4 +1,7 @@
-import { Match } from '../models/match';
+import {
+    Match, MatchInfo, TeamScore, PickBan, Objectives,
+    PlayerPerformance, Games, MatchSummary
+} from '../models/match';
 
 import pool from '../mysql';
 
@@ -50,29 +53,148 @@ const MatchService = {
         return rows as Match[];
     },
 
-    async getOne(matchId: number): Promise<Match | null> {
-        const [rows] = await pool.execute(`
-            select m.id as id,
-            t.id as tournament_id,
-            t.league as tournament,
+    async getOne(id: number): Promise<MatchSummary | null> {
+        const [rows] = await pool.query(`select 
+            m.id as matchId,
+            t.id as tournamentId,
+            t.league as league,
             t.split as split,
+            t.year as year,
             m.date as date,
-            t1.id as team_one_id,
-            t1.name as team_one_name,
-            t2.id as team_two_id,
-            t2.name as team_two_name,
-            m.playoffs as playoffs,
             m.patch as patch
             from matches as m
             left join tournaments as t
-            on t.id = m.tournament_id 
-            left join teams as t1
-            on t1.id = m.team_one
-            left join teams as t2
-            on t2.id = m.team_two 
-            where m.id = ?`, [matchId]);
-        const matches = rows as Match[];
-        return matches.length > 0 ? matches[0] : null;
+                on t.id = m.tournament_id
+            where m.id = ?;`, [id]);
+
+        const [teamScores] = await pool.query(`SELECT DISTINCT
+                team.id AS teamId,
+                team.name AS teamName,
+                team.year AS year,
+                SUM(CASE WHEN g.blue_team = team.id THEN g.blue_win ELSE 0 END) AS blueWins,
+                SUM(CASE WHEN g.red_team = team.id AND g.blue_win = 0 THEN 1 ELSE 0 END) AS redWins
+            from matches AS m 
+            LEFT JOIN games AS g
+                ON g.match_id = m.id
+            LEFT JOIN teams AS team
+                ON team.id IN (m.team_one, m.team_two)
+            WHERE m.id = ?
+            GROUP BY team.id, team.name, team.year;`, [id]);
+
+        const [objectives] = await pool.query(`select 
+            g.id as gameId,
+            g.match_id as matchId,
+            g.game_number as gameNumber,
+            tb.name as blueTeamName,
+            tr.name as redTeamName, 
+            o.side as side,
+            o.first_blood as firstBlood,
+            o.first_tower as firstTower,
+            o.towers as towers,
+            o.void_grubs as voidGrubs,
+            o.rift_heralds as riftHeralds,
+            o.baron_nashors as baronNashors,
+            o.infernals,
+            o.mountains,
+            o.clouds, 
+            o.oceans, 
+            o.hextechs,
+            o.chemtechs,
+            o.elders,
+            o.feats_of_strength as featsOfStrength,
+            o.ruinous_atakhan as ruinousAtakhan,
+            o.voracious_atakhan as voraciousAtakhan
+            from games as g
+            left join objectives as o
+                on o.game_id = g.id
+            left join teams as tb
+                on tb.id = g.blue_team
+            left join teams as tr
+                on tr.id = g.red_team
+            where g.match_id = ?;`, [id]);
+
+        const [pickBans] = await pool.query(`select
+            g.id as gameId,
+            g.match_id as matchId,
+            g.game_number as gameNumber,
+            tb.name as blueTeamName,
+            tr.name as redTeamName, 
+            pb.side,
+            pb.ban1,
+            pb.ban2,
+            pb.ban3,
+            pb.ban4,
+            pb.ban5,
+            pb.pick1,
+            pb.pick2,
+            pb.pick3,
+            pb.pick4,
+            pb.pick5
+            from games as g
+            left join pick_bans as pb
+            on pb.game_id = g.id
+            left join teams as tb
+            on tb.id = g.blue_team
+            left join teams as tr
+            on tr.id = g.red_team
+            where g.match_id = ?;`, [id]);
+
+        const [playerPerformances] = await pool.query(`select
+            g.id as gameId,
+            g.match_id as matchId,
+            g.game_number as gameNumber,
+            tb.id as blueTeamId,
+            tb.name as blueTeamName,
+            tr.id as redTeamId,
+            tr.name as redTeamName,
+            p.id as playerId,
+            p.name as playerName,
+            p.team as playerTeam,
+            pp.role,
+            pp.champion,
+            pp.kills,
+            pp.deaths,
+            pp.assists,
+            pp.damage_to_champions as damageToChampions,
+            pp.wards_placed as wardsPlaced,
+            pp.wards_destroyed as wardsDestroyed,
+            pp.control_wards_bought as controlWardsBought,
+            pp.vision_score as visionScore,
+            pp.total_gold as totalGold,
+            pp.gold_spent as goldSpent,
+            pp.creep_score as creepScore,
+            pp.kills_at_15 as killsAt15,
+            pp.deaths_at_15 as deathsAt15,
+            pp.assists_at_15 as assistsAt15,
+            pp.gold_at_15 as goldAt15
+            from games as g
+            left join player_performances as pp
+            on pp.game_id = g.id
+            left join teams as tb
+            on tb.id = g.blue_team
+            left join teams as tr
+            on tr.id = g.red_team
+            left join players as p
+            on p.id = pp.player_id
+            where g.match_id = ?;`, [id]);
+
+        const MatchSummaries = rows as MatchInfo[];
+        const match = MatchSummaries.length > 0 ? MatchSummaries[0] : null;
+
+        const game: Games = {
+            pickBans: pickBans as PickBan[],
+            objectives: objectives as Objectives[],
+            playerPerformances: playerPerformances as PlayerPerformance[]
+        }
+
+        if (!match) return null
+        const matchSummary: MatchSummary = {
+            matchInfo: match,
+            teamScores: teamScores as TeamScore[],
+            games: game
+        }
+
+        return matchSummary
     },
 
     async create(match: Match): Promise<Match> {
