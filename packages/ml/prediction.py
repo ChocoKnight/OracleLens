29818a253d2e24fa6cd884fca5d490f580732951
Kevin Tan <k.tann.578@gmail.com>
2025-05-26 15:16:28 -0700

    Update prediction.py

---
+++ packages/ml/prediction.py
@@ -0,0 +1,135 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import torch
+import numpy as np
+from model import LSTMForecaster
+import requests
+
+class NNData(BaseModel):
+    sequence: list[float]
+
+class LSTMData(BaseModel):
+    sequence: list[list[float]]
+
+app = FastAPI()
+
+input_size = 237
+output_size = 237
+
+lstm_model = LSTMForecaster(input_size=input_size, hidden_size=64, num_layers=5, output_size=output_size)
+lstm_model.load_state_dict(torch.load("OracleLens\packages\ml\models\lstm_rnn_model_128_5.pth", map_location=torch.device("cpu")))
+lstm_model.eval()
+
+nn_model = torch.nn.Sequential(
+    torch.nn.Linear(236, 128),
+    torch.nn.ReLU(),
+    torch.nn.Linear(128, 128),
+    torch.nn.ReLU(),
+    torch.nn.Linear(128, 64),
+    torch.nn.ReLU(),
+    torch.nn.Linear(64, 64),
+    torch.nn.ReLU(),
+    torch.nn.Linear(64, 1)
+)
+
+nn_model.load_state_dict(torch.load("nn_model_state.pth", map_location=torch.device("cpu")))
+nn_model.eval()
+
+def team_game_vector(gameId, teamId):
+    objectives = "http://localhost:3000/api/games"
+    player_stats = "http://localhost:3000/api/playerperformances"
+
+    blue_params = {'gameId': gameId, 'side': 'Blue'}
+    red_params = {'gameId': gameId, 'side': 'Red'}
+    blue_objectives = requests.get(objectives, params=blue_params)
+    red_objectives = requests.get(objectives, params=red_params)
+    blue_player_stats = requests.get(player_stats, params=blue_params)
+    red_player_stats = requests.get(player_stats, params=red_params)
+
+    blue_objectives_df = pd.DataFrame(blue_objectives.json())
+
+    teamBlueSide = (blue_objectives_df['blue_team'].iloc[0] == teamId)
+
+    blue_objectives_df = blue_objectives_df[['blue_win', 'game_number', 'duration', 'first_blood', 'first_tower', 'towers', 'tower_plates', 'void_grubs', 'rift_heralds',
+    'baron_nashors', 'infernals', 'mountains', 'clouds', 'oceans', 'hextechs', 'chemtechs', 'elders', 'feats_of_strength',
+    'ruinous_atakhan', 'voracious_atakhan']]
+    blue_objectives_df = blue_objectives_df.astype({col: int for col in blue_objectives_df.columns if blue_objectives_df[col].dtype == 'bool'})
+    blue_objectives_df_values = blue_objectives_df.values.flatten().tolist()
+
+    blue_player_stats_df = pd.DataFrame(blue_player_stats.json())
+    blue_player_stats_df = blue_player_stats_df[['role', 'kills', 'deaths', 'assists', 'damage_to_champions', 'wards_placed', 'wards_destroyed',
+        'control_wards_bought', 'vision_score', 'total_gold', 'gold_spent', 'creep_score', 
+        'kills_at_15', 'deaths_at_15', 'assists_at_15', 'gold_at_15']].sort_values(by='role')
+    blue_player_stats_df = blue_player_stats_df.astype({col: int for col in blue_player_stats_df.columns if blue_player_stats_df[col].dtype == 'bool'})
+    blue_player_stats_df_encoded = pd.get_dummies(blue_player_stats_df, columns=['role'], drop_first=False)
+
+    blue_player_stats_df_encoded_values = blue_player_stats_df_encoded.values.flatten().tolist()
+
+    red_objectives_df = pd.DataFrame(red_objectives.json())
+    red_objectives_df = red_objectives_df[['first_blood', 'first_tower', 'towers', 'tower_plates', 'void_grubs', 'rift_heralds',
+    'baron_nashors', 'infernals', 'mountains', 'clouds', 'oceans', 'hextechs', 'chemtechs', 'elders', 'feats_of_strength',
+    'ruinous_atakhan', 'voracious_atakhan']]
+    red_objectives_df = red_objectives_df.astype({col: int for col in red_objectives_df.columns if red_objectives_df[col].dtype == 'bool'})
+    red_objectives_df_values =  red_objectives_df.values.flatten().tolist()
+
+    red_player_stats_df = pd.DataFrame(red_player_stats.json())
+    red_player_stats_df = red_player_stats_df[['role', 'kills', 'deaths', 'assists', 'damage_to_champions', 'wards_placed', 'wards_destroyed',
+        'control_wards_bought', 'vision_score', 'total_gold', 'gold_spent', 'creep_score', 
+        'kills_at_15', 'deaths_at_15', 'assists_at_15', 'gold_at_15']].sort_values(by='role')
+    red_player_stats_df = red_player_stats_df.astype({col: int for col in red_player_stats_df.columns if red_player_stats_df[col].dtype == 'bool'})
+    red_player_stats_df_encoded = pd.get_dummies(red_player_stats_df, columns=['role'], drop_first=False)
+
+    red_player_stats_df_encoded_values = red_player_stats_df_encoded.values.flatten().tolist()
+
+    if teamBlueSide:
+        vector = blue_objectives_df_values + blue_player_stats_df_encoded_values + red_objectives_df_values + red_player_stats_df_encoded_values
+    else:
+        vector = red_objectives_df_values + red_player_stats_df_encoded_values + blue_objectives_df_values + blue_player_stats_df_encoded_values
+
+    return vector
+
+def match_vector(teamId, matchId):
+    games = "http://localhost:3000/api/games"
+
+    game_params = {'matchId': matchId}
+
+    game_response = requests.get(games, params=game_params)
+    game_response_df = pd.DataFrame(game_response.json())
+
+    # print(game_response_df)
+
+    game_ids = game_response_df['id'].tolist()
+    vectors = []
+    for gameId in game_ids:
+        vector = team_game_vector(gameId, teamId)
+
+        if len(vector) == 237:
+            vectors.append(vector)
+        time.sleep(0.1)
+
+    vectors_array = np.array(vectors)
+
+    # print(vectors_array.shape)
+
+    if len(vectors) > 0:
+        vectors_array = np.array(vectors)
+        average_vector = vectors_array.mean(axis=0)
+        average_vector[0] = round(average_vector[0])
+        return average_vector
+    else:
+        print(f"No valid vectors for matchId {matchId}") 
+        return []  
+
+def getTeamLastTenMatches(teamId: int) -> LSTMData:
+    pass 
+
+@app.post("/predict")
+def predict(teamOne: int, teamTwo: int):
+
+    teamOne_data = getTeamLastTenMatches(teamOne)
+    teamTwo_data = getTeamLastTenMatches(teamTwo)
+
+    input_tensor = torch.tensor(teamOne_data, dtype=torch.float32).unsqueeze(0)
+    with torch.no_grad():
+        output = lstm_model(input_tensor)
+    return {"prediction": output.numpy().tolist()}
